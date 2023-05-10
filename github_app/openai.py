da8351c97c673dd7ad135aa7a711db219f6c167d
toshiki31 <toshiki.m.020131@outlook.jp>
2023-05-10 15:28:55 +0900

    add: review comment from openai

---
+++ github_app/openai.py
@@ -0,0 +1,73 @@
+import os
+import openai
+import requests
+
+from flask import Flask, request
+from github import Github, GithubIntegration
+
+
+app = Flask(__name__)
+# app_idを記入
+app_id = 324665
+# 秘密鍵の読み取り
+with open("./private-key.pem") as cert_file:
+    app_key = cert_file.read()
+
+# GitHub integrationのインスタンスを作成
+integration = GithubIntegration(
+    app_id,
+    app_key,
+)
+
+# @app.route("/")
+# def index():
+#     return "Hello, world!"
+
+
+@app.route("/", methods=['POST'])
+def bot():
+    payload = request.get_json()
+    #print(payload)
+    keys = payload.keys()
+    if payload['action'] == 'opened' or payload['action'] == 'reopened':
+        # if "issue" in keys:
+        #     installation_id = payload['installation']['id']
+        #     repo_full_name = payload['repository']['full_name']
+        #     issue_number = payload['issue']['number']
+        #     user = payload['issue']['user']['login']
+
+        #     access_token = integration.get_access_token(installation_id).token
+        #     github = Github(access_token)
+
+        #     repo = github.get_repo(repo_full_name)
+        #     issue = repo.get_issue(issue_number)
+
+        #     issue.create_comment('Hello, @{}!'.format(user))
+        if "pull_request" in keys:
+            installation_id = payload['installation']['id']
+            repo_full_name = payload['repository']['full_name']
+            pull_request_number = payload['pull_request']['number']
+            user = payload['pull_request']['user']['login']
+            
+            # Githubのアクセストークンを設定
+            access_token = integration.get_access_token(installation_id).token
+            github = Github(access_token)
+
+            repo = github.get_repo(repo_full_name)
+            pull_request = repo.get_pull(pull_request_number)
+
+            # プルリクエストのコードを取得
+            diff = pull_request.get_files()[0].patch
+
+            # OpenAI APIを用いてレビュー生成
+            openai.api_key=os.environ['OPENAI_API_KEY']
+            prompt = f"Please review the following code in Japanese.:\n{diff}\nReview:"
+            response = openai.Completion.create(engine="text-davinci-002", prompt=prompt, max_tokens=1024)
+            review = response.choices[0].text.strip()
+            pull_request.create_issue_comment(format(user), review)
+        
+    return "ok"
+
+#おまじない
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
